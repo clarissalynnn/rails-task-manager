f547b1d980e9aafdc0cec057f07068646f49ece7
Clarissa Lynn <clarissalynn.5.cl@gmail.com>
2024-02-20 14:44:39 +0800

    only need to refactor, clean style, and figure out how to save completed status

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,14 @@ Rails.application.routes.draw do
   # As a user, I can list tasks
   get "tasks", to: "tasks#index"
   # As a user, I can view the details of a task
-  # As a user, I can add a new task
-  # As a user, I can edit a task (mark as completed / update title & details)
+  get "tasks/new", to: "tasks#new", as: :new_task
+  get "tasks/:id", to: "tasks#show", as: :task
+  # As a user, I can add a new task (create + new)
+  # New needs to be placed before the show
+  post "tasks", to: "tasks#create"
+  # As a user, I can edit a task (mark as completed / update title & details) (GET + PATCH) (edit + update)
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
   # As a user, I can remove a task
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
 end
